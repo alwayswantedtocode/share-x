355af7c91a37c43484ec13e3a8afd0684cccf8f4
alwayswantedtocode <101648314+alwayswantedtocode@users.noreply.github.com>
2024-11-27 20:34:29 +0100

    update scroll ui

--- src/Components/Aside/SearchAside.jsx
+++ src/Components/Aside/SearchAside.jsx
@@ -4,13 +4,15 @@ import { Link } from "react-router-dom";
 const SearchAside = ({ id, fullname, username, image }) => {
   return (
     <Link to={`/profilepage/${username}`} key={id}>
-      <div className="result-content">
-        <img src={image} alt={`${fullname} profileimage`} />
-        <div className="details">
-          <p>{fullname}</p>
-          <p>{username}</p>
+
+        <div className="result-content">
+          <img src={image} alt={`${fullname} profileimage`} />
+          <div className="details">
+            <p>{fullname}</p>
+            <p>{username}</p>
+          </div>
         </div>
-      </div>
+
     </Link>
   );
 };
