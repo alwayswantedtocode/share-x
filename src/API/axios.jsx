a181786eef5e4487f3860b47510fb46991bb9299
alwayswantedtocode <101648314+alwayswantedtocode@users.noreply.github.com>
2024-12-06 17:32:11 +0100

    axois

--- src/API/axios.jsx
+++ src/API/axios.jsx
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 export default axios.create({
-  // baseURL: "https://share-x-server.onrender.com",
+  baseURL: "https://share-x-server.onrender.com",
   
-  baseURL: "http://localhost:5050",
+  // baseURL: "http://localhost:5050",
 });
 
 //  "proxy": "https://share-x-server.onrender.com"
